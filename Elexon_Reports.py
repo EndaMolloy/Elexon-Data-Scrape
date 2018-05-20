49d6c93462900f979c71d218896a67184914ad3d
EndaMolloy <enda.molloy.1@gmail.com>
2018-05-20 21:50:06 +0100

    elexon data saved to pandas dataframe

--- Elexon_Reports.py
+++ Elexon_Reports.py
@@ -1,35 +1,53 @@
 import requests
 import pandas as pd
 from lxml import etree as et
+from collections import defaultdict
 import settings
 
 #   Create dictionary of required BM reports
 bm_reports = {'B1770': ['imbalancePriceAmountGBP', 'priceCategory'],
                'B1780': ['imbalanceQuantityMAW']}
+columns = ['settlementDate', 'settlementPeriod']
+#bm_reports = {'B1780': ['imbalanceQuantityMAW']}
 
-def getReport(reportCode):
+def getXMLReport(reportCode):
 
     url = ("https://api.bmreports.com/BMRS/"+reportCode+""
     "/v1?APIKey="+settings.ELEXON_API_KEY+"&SettlementDate=2018-05-19"
-    "&Period=1&ServiceType=xml")
+    "&Period=*&ServiceType=xml")
 
     #   request for bm report
-    r = requests.get(url)
+    r = requests.get(url).content
 
-    #   convert xml string to object
-    tree = et.parse(r.content)
-    root = tree.getroot()
+    return r
 
-    print(root.tag)
+def convertToDF(xml, cols):
 
+    #   get the xml root which is "<response>"
+    root = et.fromstring(xml)
+
+    #   add specific report columns
+    columns.extend(cols)
+
+    output = defaultdict(list)
+
+    for item in root.findall("./responseBody/responseList/item"):
+        for child in item:
+            if child.tag in columns:
+                output[child.tag].append(child.text)
+
+    df = pd.DataFrame().from_dict(output)
+
+    print(df)
     return
-
 
 def main():
     bm_reports_list = [*bm_reports]
 
-    for report in bm_reports_list:
-         getReport(report)
+    #for report in bm_reports_list:
+    for report, cols in bm_reports.items():
+        xmlReport = getXMLReport(report)
+        convertToDF(xmlReport, cols)
 
 if __name__ == '__main__':
     main()
